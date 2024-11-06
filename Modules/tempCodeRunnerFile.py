497e64a18eb931996b7cc0128fc9920a495623b8
unknown <pptien.lmht@gmail.com>
2024-11-06 17:02:49 +0700

    Toi update file DataBuild

--- Modules/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
- # print("\nThông tin DataFrame:")
-    # print(data.info())
-    # print("\nSố lượng giá trị thiếu trong mỗi cột:")
-    # print(data.isna().sum())
-    # print("\nSố lượng bản ghi bị trùng lặp:")
-    # print(data.duplicated().sum())
